8b5e65a752262c08b1e267f2edfb40050d4c8a79
Emily-Philbrick <63964311+Emily-Philbrick@users.noreply.github.com>
2020-07-08 15:15:34 -0600

    Add files via upload

--- assignment.sql
+++ assignment.sql
@@ -22,10 +22,18 @@ movie_name VARCHAR(20) NOT NULL,
 );
 
 INSERT INTO Actors( actorID, actorName, MovieName)
-VALUES (1, 'Johnny Depp', 'Pirates of the Caribbean', 2, 'Penelope Cruise', 'Blow', 3, 'Harrison Ford', 'Star Wars', 4, 'Shia Lebouf', 'Transformers', 5, 'Cameron Diaz', 'Charles Angels',
+VALUES (1, 'Johnny Depp', 'Pirates of the Caribbean'),
+(2, 'Penelope Cruise', 'Blow'),
+(3, 'Harrison Ford', 'Star Wars'),
+(4, 'Shia Lebouf', 'Transformers'),
+(5, 'Cameron Diaz', 'Charles Angels'),
 
 
 INSERT INTO Directors( directorID, directorName, MovieName)
-VALUES (1, 'Alfred Hitchcock', 'Physcho', 2, 'Quentin Tarantino', 'Once upon a time in hollywood', 3, 'Stephen Spielberg', 4, 'Christopher Nolan', 'The Dark Knight', 5, 'Gore Verbinski', 'The Pirates of the Carribean',)
+VALUES (1, 'Alfred Hitchcock', 'Physcho'),
+(2, 'Quentin Tarantino', 'Once upon a time in hollywood'), 
+(3, 'Stephen Spielberg', 'ET'),
+(4, 'Christopher Nolan', 'The Dark Knight'),
+(5, 'Gore Verbinski', 'The Pirates of the Carribean'),
 
 
